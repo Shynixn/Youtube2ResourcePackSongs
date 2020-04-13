65f8fb69558ca7882457ee6dcadf54a089695455
Shynixn <shynixn@gmail.com>
2020-04-13 16:00:12 +0200

    Updated gradle file to generate shadowJar.

--- youtube2resourcepacksongs-gui/build.gradle.kts
+++ youtube2resourcepacksongs-gui/build.gradle.kts
@@ -1,3 +1,22 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow") version ("2.0.4")
+}
+
+tasks.withType<ShadowJar> {
+    archiveName = "$baseName-$version.$extension"
+    manifest {
+        attributes(mapOf("Main-Class" to "com.github.shynixn.youtube2resourcepacksongs.gui.MainKt"))
+    }
+}
+
+publishing {
+    publications {
+        (findByName("mavenJava") as MavenPublication).artifact(tasks.findByName("shadowJar")!!)
+    }
+}
+
 dependencies {
     implementation(project(":youtube2resourcepacksongs-api"))
     implementation(project(":youtube2resourcepacksongs-logic"))
